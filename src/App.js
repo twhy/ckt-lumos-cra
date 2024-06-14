7c8ad907c0d8a0778446e2e7ba3671413a2cf7ff
Tom Wang <tom.wanghaiyi@gmail.com>
2024-06-14 15:51:15 +0800

    import and expose lumos

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Open console and type <code>lumos</code> to try some Lumos APIs.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ckb-js/lumos"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lumos on GitHub
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as lumos from 'ckt-lumos-lumos';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Object.assign(window, { lumos });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
